1dbd03327d04a9ecc274b063b08f4c13fb49ce2f
Web <seisakukun@mail.iiiexhibition.com>
2012-06-10 05:43:38 +0900

    add ParserTrait

--- src/main/scala/ParserSelector.scala
+++ src/main/scala/ParserSelector.scala
@@ -9,6 +9,30 @@
 package com.takanori.MovieUtils
 
 import com.takanori.scalamovie.MP4Parser
+import java.io._
+import org.apache.tika.sax.BodyContentHandler
+import org.apache.tika.metadata.Metadata
+import org.apache.tika.parser._
+import org.xml.sax.ContentHandler
+
+
+trait ParserTrait extends AbstractParser {
+
+  def parse(stream: InputStream, handler: ContentHandler, metadata: Metadata,
+            parseContext: ParseContext)
+
+  // TODO ここでAbstractParserの関数をすべて実装しておく
+
+}
+
+//class TestParser extends ParserTrait {
+//
+//  def parse(stream: InputStream, handler: ContentHandler, metadata: Metadata,
+//            parseContext: ParseContext) = {
+//    println("TestParser.parse")
+//  }
+//
+//}
 
 class ParserSelector {
 
@@ -16,6 +40,7 @@ class ParserSelector {
 
     format match {
       case MovieFormat.MP4 | MovieFormat.MOV | MovieFormat.ThreeGP => Some(new MP4Parser)
+//      case MovieFormat.Unknown => Some(new TestParser)
       case _ => None
     }
 
